61acfc8198fd4c06a0f33cd57d261a6c1f8d4b27
nanomolina <nanomolinacav@gmail.com>
2023-07-25 16:49:53 -0300

    v1.0.2

--- macos/Formula/nde.rb
+++ macos/Formula/nde.rb
@@ -1,7 +1,7 @@
 class Nde < Formula
   desc "A script to display a falling text animation in the terminal of Nacidos del Error"
   homepage "https://github.com/nanomolina/nde"
-  url "https://github.com/nanomolina/nde/releases/download/v1.0.1/nde-v1.0.1.tar.gz"
+  url "https://github.com/nanomolina/nde/releases/download/v1.0.2/nde-v1.0.2.tar.gz"
   sha256 "f73f7ae767b430b9d8e2a35805d94907100bd3d306bfcd29a3186e8073da0a42"
 
   def install

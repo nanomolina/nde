dae47641446d87ba1a0f24ac35c46e0c7fb90652
nanomolina <nanomolinacav@gmail.com>
2023-07-25 15:31:12 -0300

    update rb fgile

--- nde.rb
+++ nde.rb
@@ -1,11 +1,11 @@
 class Nde < Formula
   desc "A script to display a falling text animation in the terminal of Nacidos del Error"
   homepage "https://github.com/nanomolina/nde"
-  url "https://github.com/nanomolina/nde/releases/download/v1.0.0/nde-scripts-v1.0.0.tar.gz" # Replace with the actual URL to your GitHub repository and the desired version
+  url "https://github.com/nanomolina/nde/releases/download/v1.0.0/nde-scripts-v1.0.0.tar.gz"
   sha256 "dba339de2fa9c4693ec451065311fcc70d5f5c8217e9a6b1784088ef567feb79"
 
   def install
-    bin.install "scripts/nde" # Replace "nde.rb" with the actual filename of your script
+    bin.install "nde" # Corrected path to the script file
   end
 
   test do

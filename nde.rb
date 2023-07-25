54a5e357502c2097702fdfd07a8fef76dd71e363
nanomolina <nanomolinacav@gmail.com>
2023-07-25 14:59:37 -0300

    update rb file

--- nde.rb
+++ nde.rb
@@ -1,14 +1,14 @@
 class Nde < Formula
-  desc "A script to display a falling text animation in the terminal"
+  desc "A script to display a falling text animation in the terminal of Nacidos del Error"
   homepage "https://github.com/nanomolina/nde"
-  url "https://github.com/your-username/your-repo/archive/v1.0.0.tar.gz" # Replace with the actual URL to your GitHub repository and the desired version
-  sha256 "dba339de2fa9c4693ec451065311fcc70d5f5c8217e9a6b1784088ef567feb79  nde-scripts-v1.0.0.tar.gz" # Get the SHA256 sum by running "shasum -a 256 v1.0.0.tar.gz" (replace with the actual filename and version)
+  url "https://github.com/nanomolina/nde/releases/download/v1.0.0/nde-scripts-v1.0.0.tar.gz" # Replace with the actual URL to your GitHub repository and the desired version
+  sha256 "dba339de2fa9c4693ec451065311fcc70d5f5c8217e9a6b1784088ef567feb79"
 
   def install
-    bin.install "nde" # Replace "nde" with the actual filename of your script
+    bin.install "nde.rb" # Replace "nde.rb" with the actual filename of your script
   end
 
   test do
-    system "nde --version" # Replace "--version" with the appropriate flag to show version information
+    system "nde.rb --version" # Replace "--version" with the appropriate flag to show version information
   end
 end

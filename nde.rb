8fdc870d5cef60baa1c4c539394188b879eb1551
nanomolina <nanomolinacav@gmail.com>
2023-07-25 14:52:25 -0300

    recreate archive

--- nde.rb
+++ nde.rb
@@ -2,7 +2,7 @@ class Nde < Formula
   desc "A script to display a falling text animation in the terminal"
   homepage "https://github.com/nanomolina/nde"
   url "https://github.com/your-username/your-repo/archive/v1.0.0.tar.gz" # Replace with the actual URL to your GitHub repository and the desired version
-  sha256 "replace-with-sha256-sum" # Get the SHA256 sum by running "shasum -a 256 v1.0.0.tar.gz" (replace with the actual filename and version)
+  sha256 "dba339de2fa9c4693ec451065311fcc70d5f5c8217e9a6b1784088ef567feb79  nde-scripts-v1.0.0.tar.gz" # Get the SHA256 sum by running "shasum -a 256 v1.0.0.tar.gz" (replace with the actual filename and version)
 
   def install
     bin.install "nde" # Replace "nde" with the actual filename of your script
